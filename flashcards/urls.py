55623f9ac337bf2a6973b3d8884701f88139dfe9
Ajsteph89 <ajsteph89@gmail.com>
2022-08-27 11:51:25 -0400

    new subject able to be added, stil working on home button

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_subject, name='list_subject'),
+    path('subject/new/', views.subject_new, name='subject_new'),
+    path('', views.base, name='base'),
 ]    
--- flashcards/views.py
+++ flashcards/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-
+import flashcards
 from flashcards.models import Flashcard, Subject
+from .forms import SubjectForm
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def base(request):
+    return render(request, 'flashcards/base.html')
 
 def list_subject(request):
     subjects = Subject.objects.all()
     return render(request, 'flashcards/list_subject.html', {"subjects": subjects})
+
+def subject_new(request):
+    if request.method == "POST":
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            subject = form.save(commit=False)
+            subject.save()
+            return redirect('base')
+    else:
+        form = SubjectForm()
+    return render(request, 'flashcards/subject_edit.html', {'form': form})
