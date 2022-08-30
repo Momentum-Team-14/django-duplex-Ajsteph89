a464d21d253838061c68fac51b97246963595269
Ajsteph89 <ajsteph89@gmail.com>
2022-08-30 11:12:27 -0400

    added edit function to card & subject

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.list_subject, name='list_subject'),
     path('subject/new/', views.subject_new, name='subject_new'),
+    path('subject/edit/<int:pk>', views.subject_edit, name='subject_edit'),
     path('delete_subject/<int:pk>', views.delete_subject, name='delete_subject'),
     path('card_questions/<int:pk>', views.card_questions, name='card_questions'),
     path('flashcard/new/<int:pk>', views.card_new, name='card_new'),
+    path('flashcard/edit/<int:pk1>/<int:pk2>', views.card_edit, name='card_edit'),
     path('delete_card/<int:pk1>/<int:pk2>', views.delete_card, name='delete_card'),
 ]
--- flashcards/views.py
+++ flashcards/views.py
@@ -23,6 +23,18 @@ def subject_new(request):
     return render(request, 'flashcards/subject_new.html', {'subject_form': subject_form})
 
 
+def subject_edit(request, pk):
+    subject = get_object_or_404(Subject, pk=pk)
+    if request.method == "POST":
+        subject_form = SubjectForm(request.POST, instance=subject)
+        if subject_form.is_valid():
+            subject = subject_form.save(commit=False)
+            subject.save()
+            return redirect('list_subject')
+    else:
+        subject_form = SubjectForm(instance=subject)
+    return render(request, 'flashcards/subject_edit.html', {'subject_form': subject_form})
+
 
 def card_questions(request, pk):
     subject = get_object_or_404(Subject, pk=pk)
@@ -49,6 +61,22 @@ def card_new(request, pk=None):
     else:
         card_form = FlashcardForm()
     return render(request, 'flashcards/card_new.html', {'card_form': card_form})
+
+
+def card_edit(request, pk1, pk2):
+    card = get_object_or_404(Flashcard, pk=pk1)
+    subject = get_object_or_404(Subject, pk=pk2)
+    if request.method == "POST":
+        card_form = FlashcardForm(request.POST, instance=card)
+        if card_form.is_valid():
+            card = card_form.save(commit=False)
+            card.subject = subject
+            card.user = request.user
+            card.save()
+            return redirect('card_questions', pk=pk2)
+    else:
+        card_form = FlashcardForm(instance=card)
+    return render(request, 'flashcards/card_edit.html', {'card_form': card_form})
 
 
 def delete_card(request, pk1, pk2):
